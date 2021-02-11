2d4d98ca7d64a8079c1e94e6710c8dfefee56052
Luiz Schinemann <luizschinemann@gmail.com>
2021-02-11 15:17:22 -0300

    Removing language package

--- config/admin.php
+++ config/admin.php
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>JB</b>',
+    'logo-mini' => '<b>JOB</b>',
 
     /*
     |--------------------------------------------------------------------------
@@ -395,26 +395,6 @@ return [
     |
     */
     'extensions' => [
-
-        'extensions' => [
-            'multi-language' => [
-                'enable' => false,
-                // the key should be same as var locale in config/app.php
-                // the value is used to show
-                'languages' => [
-                    'en' => 'English',
-                    'pt-br' => 'Português',
-                ],
-                // default locale
-                'default' => 'pt-br',
-                // if or not show multi-language login page, optional, default is true
-                'show-login-page' => true,
-                // if or not show multi-language navbar, optional, default is true
-                'show-navbar' => true,
-                // the cookie name for the multi-language var, optional, default is 'locale'
-                'cookie-name' => 'locale'
-            ],
-        ],
 
     ],
 ];
